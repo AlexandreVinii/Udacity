b94dc6b8299c055d697d650fbaedfc8afb04f000
Alexandre <alexandreviniciusfs@gmail.com>
2018-08-22 00:13:37 -0300

    Aula 21/08

--- Coding/exerc01.py
+++ Coding/exerc01.py
@@ -1,6 +1,7 @@
+
 usernames = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
 
 for i in range(len(usernames)):
-    usernames[i] = usernames[i].lower().replace(" ", "_")
+    usernames[i] = usernames[i].upper().replace(" ", "_")
 
 print(usernames)
