3e1d677fdaf8889dc7a73ae87895ef0c2a3c0eb0
Alexandre <alexandreviniciusfs@gmail.com>
2018-08-23 15:19:04 -0300

    23/08 - Loops pause - continue

--- Coding/exerc01.py
+++ Coding/exerc01.py
@@ -1,7 +1,39 @@
 
-usernames = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+manifest = [("banana", 10),("Ariana Serafim", 61),
+            ("cheese", 28), ("Machine", 210), ("amoeba", 300)]
 
-for i in range(len(usernames)):
-    usernames[i] = usernames[i].upper().replace(" ", "_")
+weight = 0
+items = []
+for cargo_name, cargo_weight in manifest:
+    print("current Weight :  {}".format(weight))
+    if weight >= 100:
+        print("Breaking now !")
+        break
 
-print(usernames)
+    else:
+        print("adding {} ({})".format(cargo_name, cargo_weight))
+        items.append(cargo_name)
+        weight += cargo_weight
+
+print("\nFinal Weight: {}".format(weight))
+print("Final Items: {}".format(items))
+
+print("\nMETHOD 2")
+weight = 0
+items = []
+for cargo_name, cargo_weight in manifest:
+    print("current weight: {}".format(weight))
+    print("current cargo_weight: {}".format(cargo_weight))
+    if weight >= 100:
+        print("  breaking from the loop now!")
+        break
+    elif weight + cargo_weight > 100:
+        print("  skipping {} ({})".format(cargo_name, cargo_weight))
+        continue
+    else:
+        print("  adding {} ({})".format(cargo_name, cargo_weight))
+        items.append(cargo_name)
+        weight += cargo_weight
+
+print("\nFinal Weight: {}".format(weight))
+print("Final Items: {}".format(items))
