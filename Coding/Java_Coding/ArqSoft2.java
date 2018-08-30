80d4a3e68a753fa9da8356e8f4d1d4a6596f6ad4
Alexnadre Vinicius <alexandreviniciusfs@gmail.com>
2018-08-30 20:03:30 -0300

    Update 30/08 Nt

---
+++ Coding/Java_Coding/ArqSoft2.java
@@ -0,0 +1,12 @@
+
+public class ArqSoft2 {
+
+    public static void main(String[] args) {
+        
+    
+    }
+
+
+    
+}
+
---
+++ Coding/Java_Coding/ILoja.java
@@ -0,0 +1,7 @@
+public class ILoja{
+
+    public static void getInfo(){
+        
+    }
+    
+}
